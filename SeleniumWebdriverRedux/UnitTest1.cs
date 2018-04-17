ebf27f681f84b9cd90e350df469e2e89f55ad9e4
MarkCTest <cyreath@gmail.com>
2018-04-17 23:55:55 +0100

    Core Driver set-up

---
+++ SeleniumWebdriverRedux/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumWebdriverRedux
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var driver = GetChromeDriver();
+            driver.Navigate().GoToUrl("http://www.cyreath.co.uk");
+        }
+
+        private IWebDriver GetChromeDriver()
+        {
+            // Get the location of the path for the chrome driver dynamically
+            var outputDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            return new ChromeDriver(outputDirectory);
+        }
+    }
+}
